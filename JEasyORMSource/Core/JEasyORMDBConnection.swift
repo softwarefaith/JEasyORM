decd65aaacc3bdf795085b7324b545f0c62a5829
蔡杰 <jie.cai@mljr.com>
2017-07-04 00:03:50 +0800

    创建orm 工程

---
+++ JEasyORMSource/Core/JEasyORMDBConnection.swift
@@ -0,0 +1,26 @@
+//
+//  JEasyORMDBConnection.swift
+//  JEasyORM
+//
+//  Created by 蔡杰 on 2017/7/3.
+//  Copyright © 2017年 蔡杰. All rights reserved.
+//
+
+import UIKit
+
+public final class JEasyORMDBConnection {
+    
+
+    enum StorageMode: CustomStringConvertible{
+        
+
+        var description: String {
+            return ""
+        }
+    }
+    
+    enum Operation {
+        
+    }
+
+}
