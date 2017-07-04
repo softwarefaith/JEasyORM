2093ff7f511ff7ea3bee85549f1012ded9f79fef
蔡杰 <jie.cai@mljr.com>
2017-07-04 15:10:49 +0800

    1.添加bridge 2.定义 数据库 存储类型 以及 操作方式

---
+++ JEasyBridge.h
@@ -0,0 +1,14 @@
+//
+//  JEasyBridge.h
+//  JEasyORM
+//
+//  Created by 蔡杰 on 2017/7/4.
+//  Copyright © 2017年 蔡杰. All rights reserved.
+//
+
+#ifndef JEasyBridge_h
+#define JEasyBridge_h
+
+#import <sqlite3.h>
+
+#endif /* JEasyBridge_h */
--- JEasyORMSource/Core/JEasyORMDBConnection.swift
+++ JEasyORMSource/Core/JEasyORMDBConnection.swift
@@ -10,16 +10,49 @@ import UIKit
 
 public final class JEasyORMDBConnection {
     
-
+//定义数据库存储方式(枚举、常量)
     enum StorageMode: CustomStringConvertible{
         
+        case inMemory
+        case temporary
+        case uri(String)
 
         var description: String {
-            return ""
+            
+            switch self {
+                case .inMemory:
+                    return ":memory:"
+                case .temporary:
+                    return ""
+                case .uri(let path):
+                    return path;
+            }
         }
     }
     
     enum Operation {
+        
+        case insert
+        case update
+        case delete
+        case select
+        
+        //将数据库类型->转成枚举类型
+        fileprivate init(value:Int32){
+            switch value {
+                case SQLITE_INSERT:
+                    self = .insert
+                case SQLITE_UPDATE:
+                    self = .update
+                case SQLITE_DELETE:
+                    self = .delete
+                case SQLITE_SELECT:
+                    self = .select
+                default:
+                    fatalError("没有这个类型:\(value)")
+            }
+        }
+
         
     }
 
